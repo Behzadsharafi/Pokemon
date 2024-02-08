6be926e027267cff2fbb38e196f5cd9b1c7c28ac
Behzad Sharafi <behzadsharafi@gmail.com>
2024-02-08 23:35:39 +1100

    updates background image

--- pokemon-frontend/src/App.tsx
+++ pokemon-frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import AppContainer from "./containers/AppContainer/AppContainer";
 import "./global.scss";
-import backgroundImage from "../src/assets/Pokemon.jpg";
 
 function App() {
   return (
     <>
-      <img className="backgroundImage" src={backgroundImage} alt="" />
       <AppContainer />
     </>
   );
